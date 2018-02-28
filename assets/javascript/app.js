669b5837eb07af0647e84b422ef4c186e33130e9
jphamxuan <34908420+jphamxuan@users.noreply.github.com>
2018-02-27 22:50:52 -0800

    added some js stuff

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,13 +1,13 @@
 $( document ).ready(function() {
     
     // An array of actions, new actions will be pushed into this array;
-    var buttons = ["Dancing", "Jogging", "Falling", "Reading", "Pushing", "Swimming", "Eating", "Skipping", "Crying", "Winking","Beyoncing", "Strolling", "Hopping"];
+    var buttons = ["lol", "cool", "bro", "dude", "dawg", "coding", "friday", "yay", "dogs", "wut","meme"];
    
    
     //makes buttons from array
 
     function displayGifButtons(){
-        $("#gifButtonsView").empty(); // erasing anything in this div id so that it doesnt duplicate the results
+        $("#gifButtonsView").empty(); 
         for (var i = 0; i < buttons.length; i++){
             var gifButton = $("<button>");
             gifButton.addClass("buttons");
@@ -17,71 +17,5 @@ $( document ).ready(function() {
             $("#gifButtonsView").append(gifButton);
         }
     }
-    // Function to add a new action button
-    function addNewButton(){
-        $("#addGif").on("click", function(){
-        var action = $("#action-input").val().trim();
-        if (action == ""){
-          return false; // added so user cannot add a blank button
-        }
-        buttons.push(action);
-    
-        displayGifButtons();
-        return false;
-        });
-    }
-  
-    // Function that displays all of the gifs
-    function displayGifs(){
-        var action = $(this).attr("data-name");
-        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + action + "&api_key=dc6zaTOxFJmzC&limit=10";
-        console.log(queryURL); // displays the constructed url
-        $.ajax({
-            url: queryURL,
-            method: 'GET'
-        })
-        .done(function(response) {
-            console.log(response); // console test to make sure something returns
-            $("#gifsView").empty(); // erasing anything in this div id so that it doesnt keep any from the previous click
-            var results = response.data; //shows results of gifs
-            if (results == ""){
-              alert("There isn't a gif for this selected button");
-            }
-            for (var i=0; i<results.length; i++){
-    
-                var gifDiv = $("<div>"); //div for the gifs to go inside
-                gifDiv.addClass("gifDiv");
-                // pulling rating of gif
-                var gifRating = $("<p>").text("Rating: " + results[i].rating);
-                gifDiv.append(gifRating);
-                // pulling gif
-                var gifImage = $("<img>");
-                gifImage.attr("src", results[i].images.fixed_height_small_still.url); // still image stored into src of image
-                gifImage.attr("data-still",results[i].images.fixed_height_small_still.url); // still image
-                gifImage.attr("data-animate",results[i].images.fixed_height_small.url); // animated image
-                gifImage.attr("data-state", "still"); // set the image state
-                gifImage.addClass("image");
-                gifDiv.append(gifImage);
-                // pulling still image of gif
-                // adding div of gifs to gifsView div
-                $("#gifsView").prepend(gifDiv);
-            }
-        });
-    }
-    // Calling Functions & Methods
-    displayGifButtons(); // displays list of actions already created
-    addNewButton();
-    removeLastButton();
-    // Document Event Listeners
-    $(document).on("click", ".action", displayGifs);
-    $(document).on("click", ".image", function(){
-        var state = $(this).attr('data-state');
-        if ( state == 'still'){
-            $(this).attr('src', $(this).data('animate'));
-            $(this).attr('data-state', 'animate');
-        }else{
-            $(this).attr('src', $(this).data('still'));
-            $(this).attr('data-state', 'still');
-        }
-    });
-    });
+   
+   
