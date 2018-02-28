b8b434d20c1612c60a346b8021305f0fef3856c5
jphamxuan <34908420+jphamxuan@users.noreply.github.com>
2018-02-27 23:34:34 -0800

    api key added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -22,13 +22,15 @@ $( document ).ready(function() {
     displayGifButtons();
 });
    
-// //displays gifs
-//     function displayGifs(){
-//         var action = $(this).attr("data-name");
-//         var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + action + "&api_key=dc6zaTOxFJmzC&limit=10";
-//         console.log(queryURL); // displays the constructed url
-//         $.ajax({
-//             url: queryURL,
-//             method: 'GET'
-//         })
+//displays gifs
+    function displayGifs(){
+        var action = $(this).attr("data-name");
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + action + "&api_key=zDcx4n31eEurSTfxNLb5LAeCmiJJeqDD";
+        console.log(queryURL); // displays the constructed url
+        $.ajax({
+            url: queryURL,
+            method: 'GET'
+        })
+
+        
         
