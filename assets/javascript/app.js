017768dcced4da7fea4b89abe1b63a48a132a38d
jphamxuan <34908420+jphamxuan@users.noreply.github.com>
2018-02-27 23:25:38 -0800

    got buttons to appear

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,31 +1,34 @@
 $( document ).ready(function() {
     
-    // An array of actions, new actions will be pushed into this array;
-    var buttons = ["lol", "cool", "bro", "dude", "dawg", "coding", "friday", "yay", "dogs", "wut","meme"];
+    //button array 
+
+    var buttons = ["lol", "cool", "bro", "dude", "dawg", "coding", "friday", "yay", "dogs", "wut",];
    
    
     //makes buttons from array
 
     function displayGifButtons(){
-        $("#gifButtonsView").empty(); 
+        $("#buttonDiv").empty(); 
         for (var i = 0; i < buttons.length; i++){
             var gifButton = $("<button>");
             gifButton.addClass("buttons");
             gifButton.addClass("btn btn-primary")
             gifButton.attr("data-name", buttons[i]);
             gifButton.text(buttons[i]);
-            $("#gifButtonsView").append(gifButton);
+            $("#buttonDiv").append(gifButton);
         }
     }
+
+    displayGifButtons();
+});
    
-//displays gifs
-    function displayGifs(){
-        var action = $(this).attr("data-name");
-        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + action + "&api_key=dc6zaTOxFJmzC&limit=10";
-        console.log(queryURL); // displays the constructed url
-        $.ajax({
-            url: queryURL,
-            method: 'GET'
-        })
+// //displays gifs
+//     function displayGifs(){
+//         var action = $(this).attr("data-name");
+//         var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + action + "&api_key=dc6zaTOxFJmzC&limit=10";
+//         console.log(queryURL); // displays the constructed url
+//         $.ajax({
+//             url: queryURL,
+//             method: 'GET'
+//         })
         
-   
